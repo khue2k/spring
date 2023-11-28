c2278edea45968cea21c37ad5a1bf0378c94d163
khue2k <songkhecole@gmail.com>
2023-11-28 08:05:22 +0700

    a

--- src/main/java/com/example/springsecurity/websocket/HandMessage.java
+++ src/main/java/com/example/springsecurity/websocket/HandMessage.java
@@ -2,13 +2,25 @@ package com.example.springsecurity.websocket;
 
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 @Slf4j
 public class HandMessage extends TextWebSocketHandler {
-    private final String url = "https://api.openai.com/v1/chat/completions";
+    private final String URL = "https://api.openai.com/v1/chat/completions";
+    private final String API_KEY = "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";
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
@@ -17,8 +29,19 @@ public class HandMessage extends TextWebSocketHandler {
         session.sendMessage(new TextMessage("Hello client !"));
     }
 
-    private String getMessageResponse(String messageRequest) {
-        Gson gson = new Gson();
-        return null;
+    private String getMessageResponse(String messageRequest) throws UnsupportedEncodingException {
+        try {
+            Gson gson = new Gson();
+            HttpClient httpClient = HttpClients.createDefault();
+            HttpPost request = new HttpPost(URL);
+            request.addHeader("Authorization", "Bearer " + API_KEY);
+            StringEntity params = new StringEntity(messageRequest);
+            request.setEntity(params);
+            HttpResponse httpResponse = httpClient.execute(request);
+            HttpEntity entity = httpResponse.getEntity();
+            return EntityUtils.toString(entity, "UTF-8");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
