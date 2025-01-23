e483c5c597f184779449d4597ce79d2f78ade62b
khue29 <duongxuankhue@vnpt.vn>
2025-01-23 08:26:24 +0700

    update

--- src/main/java/com/example/springsecurity/test/Test.java
+++ src/main/java/com/example/springsecurity/test/Test.java
@@ -1,30 +1,7 @@
 package com.example.springsecurity.test;
 
-import java.util.Collections;
-
-class A {
-    void m1() throws ArrayIndexOutOfBoundsException {
-        System.out.println("AAA");
-    }
-}
-
-class B extends A {
-    void m1() throws IndexOutOfBoundsException {
-        System.out.println("BBB");
-    }
-}
-
 public class Test {
     public static void main(String[] args) {
-        int result, x;
-        x = 1;
-        result = 0;
-        while (x <= 10) {
-            if (x % 2 == 0)
-                result += x;
-            x++;
-        }
-        System.out.println(result);
-
+        System.out.println("Starting");
     }
 }
