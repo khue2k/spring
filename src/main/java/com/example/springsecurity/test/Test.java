4757feb8f24a371f6cb1f9ddf0f0858f2c4d9c1e
khue29 <duongxuankhue@vnpt.vn>
2025-02-02 15:03:49 +0700

    fix bug

--- src/main/java/com/example/springsecurity/test/Test.java
+++ src/main/java/com/example/springsecurity/test/Test.java
@@ -1,7 +1,19 @@
 package com.example.springsecurity.test;
 
+import java.util.TreeSet;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Starting");
+        TreeSet<Integer> s = new TreeSet<>();
+        TreeSet<Integer> subSet;
+        for (int i = 606; i < 613; i++) {
+            if (i % 2 == 0) {
+                s.add(i);
+            }
+        }
+        subSet = (TreeSet<Integer>) s.subSet(608, true, 611, false  );
+        s.add(609);
+
+        System.out.println(s + " " + subSet);
     }
 }
