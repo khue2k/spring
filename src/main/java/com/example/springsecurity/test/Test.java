5de06662d6e5a60e5a738f1d5f37ebcb5f700463
khue29 <duongxuankhue@vnpt.vn>
2025-02-15 18:20:13 +0700

    upadata

--- src/main/java/com/example/springsecurity/test/Test.java
+++ src/main/java/com/example/springsecurity/test/Test.java
@@ -4,16 +4,6 @@ import java.util.TreeSet;
 
 public class Test {
     public static void main(String[] args) {
-        TreeSet<Integer> s = new TreeSet<>();
-        TreeSet<Integer> subSet;
-        for (int i = 606; i < 613; i++) {
-            if (i % 2 == 0) {
-                s.add(i);
-            }
-        }
-        subSet = (TreeSet<Integer>) s.subSet(608, true, 611, false  );
-        s.add(609);
-
-        System.out.println(s + " " + subSet);
+        System.out.println(Integer.parseInt("1aa"));
     }
 }
